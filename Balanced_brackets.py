db18664be396409c0434b7eb501d100c921dd867
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-07 16:55:13 +0100

    Add function with basic checks for wrong string of brackets

--- Balanced_brackets.py
+++ Balanced_brackets.py
@@ -7,13 +7,44 @@ def get_user_brackets():
     return user_brackets
 
 
+def check_equality_of_brackets(bracket_string):
+    left_bracket = 0
+    right_bracket = 0
+    for i in list(bracket_string):
+        if i == '[':
+            left_bracket += 1
+        elif i == ']':
+            right_bracket += 1
+
+    if left_bracket == right_bracket:
+        result = True
+    else:
+        result = False
+
+    return result
+
+
 def check_user_brackets(bracket_string):
-    
+    if len(bracket_string) % 2 != 0:
+        # print('Nieparzyste')
+        result = False
+    elif bracket_string[0] == ']':
+        # print('prawy na poczatku')
+        result = False
+    elif bracket_string[-1] == '[':
+        # print('lewy na koncu')
+        result = False
+    elif check_equality_of_brackets(bracket_string) is False:
+        # print('nierowna liczba bracketow')
+        result = False
+
+    return result
 
 
 def main():
     input_brackets = get_user_brackets()
     brackets_check_result = check_user_brackets(input_brackets)
+    print(brackets_check_result)
 
 
 if __name__ == '__main__':
