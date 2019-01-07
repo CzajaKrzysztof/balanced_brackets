0f68c22f1db821e8c70a0e15a74f8dedceab80ad
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-07 18:14:34 +0100

    Upgrade function get_user_brackets() to check for apropriet input. Remove test prints. Remove redundant chec function check_equality_of_brackets(). Add function to print out final result.

--- Balanced_brackets.py
+++ Balanced_brackets.py
@@ -1,25 +1,17 @@
 def get_user_brackets():
-    """ Function get user input as string of '[' and ']' brackets. """
-    user_brackets = input("Please enter string of brackets '[' or ']': ")
+    """
+    Function get user input as string of '[' and ']' brackets.
+    Function checks if user input have only [ and ] brackets.
+    """
+    while True:
+        user_brackets = input("Please enter string of brackets '[' or ']': ")
+
+        if all(char in ['[', ']'] for char in user_brackets):
+            break
+        else:
+            print("Input can only consist of [ and ] brackets. Try again.")
+
     return user_brackets
-
-
-def check_equality_of_brackets(bracket_string):
-    """ Function check if amount of '[' and ']' brackets are equal. """
-    left_bracket = 0
-    right_bracket = 0
-    for i in list(bracket_string):
-        if i == '[':
-            left_bracket += 1
-        elif i == ']':
-            right_bracket += 1
-
-    if left_bracket == right_bracket:
-        result = True
-    else:
-        result = False
-
-    return result
 
 
 def advanced_bracket_check(bracket_string):
@@ -52,19 +44,12 @@ def check_user_brackets(bracket_string):
     Returns False if any check fails.
     """
     if len(bracket_string) % 2 != 0:
-        print('Nieparzyste')
         result = False
     elif bracket_string[0] == ']':
-        print('prawy na poczatku')
         result = False
     elif bracket_string[-1] == '[':
-        print('lewy na koncu')
-        result = False
-    elif check_equality_of_brackets(bracket_string) is False:
-        print('nierowna liczba bracketow')
         result = False
     elif advanced_bracket_check(bracket_string) is False:
-        print('zaawansowane szukanie')
         result = False
     else:
         result = True
@@ -72,13 +57,18 @@ def check_user_brackets(bracket_string):
     return result
 
 
-def main():
-    input_brackets = get_user_brackets()
-    brackets_check_result = check_user_brackets(input_brackets)
+def show_final_result(brackets_check_result, input_brackets):
+    """ Prints out result of checking given string """
     if brackets_check_result:
         print('String of brackets: {0} is OK'.format(input_brackets))
     else:
         print('String of brackets: {0} is NOT OK'.format(input_brackets))
+
+
+def main():
+    input_brackets = get_user_brackets()
+    brackets_check_result = check_user_brackets(input_brackets)
+    show_final_result(brackets_check_result, input_brackets)
 
 
 if __name__ == '__main__':
