af75020373492f260727d93aa0bdd085d39080d9
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-07 16:18:16 +0100

    Add function get string of brackets from user.

---
+++ Balanced_brackets.py
@@ -0,0 +1,20 @@
+# TODO: zestaw nawiasow mowi miec tyle samo otwierajacych co zamykajacych nawiasow.
+# TODO: ciag nawiasow nie moze zawierac ] na poczatku stringu, [ na koncu stringu.
+
+
+def get_user_brackets():
+    user_brackets = input("Please enter string of brackets '[' or ']': ")
+    return user_brackets
+
+
+def check_user_brackets(bracket_string):
+    
+
+
+def main():
+    input_brackets = get_user_brackets()
+    brackets_check_result = check_user_brackets(input_brackets)
+
+
+if __name__ == '__main__':
+    main()
